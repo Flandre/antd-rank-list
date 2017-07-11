650bb29b75c5ac68bee31011ebf8d6f14d5b03b4
Flandre <flantan233@gmail.com>
2017-07-11 17:58:01 +0800

    update

--- server.js
+++ server.js
@@ -1,23 +1,21 @@
-const webpack = require('webpack');
-const webpackDevMiddleware = require('webpack-dev-middleware');
-const webpackHotMiddleware = require('webpack-hot-middleware');
-const config = require('./webpack.config');
-
-const app = new (require('express'))();
+const express = require('express')
+const app = new express();
+const path = require('path')
 const port = 3000;
 
-const compiler = webpack(config);
-app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
-app.use(webpackHotMiddleware(compiler));
+const opn = require('opn')
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + '/test.html')
 });
+
+app.use(express.static(path.join(__dirname, 'dist')))
 
 app.listen(port, function(error) {
   if (error) {
     console.error(error)
   } else {
     console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port)
+    opn('http://localhost:' + port, {app: 'chrome'})
   }
 });
