ffe2edbe4b0f8e64045c49140a750fc6e21a9c38
Flandre <flantan233@gmail.com>
2017-07-12 18:13:07 +0800

    update

--- src/components/SenkaRank.js
+++ src/components/SenkaRank.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class SenkaRank extends Component {
   constructor(){
@@ -9,17 +10,39 @@ export default class SenkaRank extends Component {
   }
 
   handleClick = e =>{
-    this.setState({
-      value: this.state.value + 2
-    }, () => {
-      console.log('click: ' + this.state.value)
-    })
+    console.log(e.currentTarget.value);
+    // axios.get('http://124.65.37.154:12450/api/calrank?server=' + e.currentTarget.value)
+    //   .then(res => {
+    //     console.log('=== axios ===')
+    //     console.log(res)
+    //   })
+    //   .catch(err => {
+    //     console.log(err)
+    //   })
+
+    fetch('http://124.65.37.154:12450/api/calrank?server=' + e.currentTarget.value)
+      .then(res => {
+        console.log('=== fetch ===')
+        console.log(res);
+        return res.text();
+      })
+      .then(res => {
+        console.log('=== fetch res ===')
+        console.log(res)
+      })
   }
 
   render(){
+    const serverList = [8, 16, 19];
     return(
       <div>
-        <button onClick={this.handleClick}>get</button>
+        {
+          serverList.map((server, index) =>
+            <button key={index} value={server} onClick={this.handleClick}>server: {server}</button>
+          )
+        }
+        <br />
+        {this.state.value}
       </div>
     )
   }
